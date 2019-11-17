72351ef50c5dff820ce4d5a5399c221562b9fd42
djwj <daniel@wilshirejones.com>
2019-11-17 14:22:06 +0000

    move timed_messenger python to own module

---
+++ timed_messenger.py
@@ -0,0 +1,17 @@
+from ctypes import *
+
+
+class TimedResponse(Structure):
+    _fields_ = [
+        ("start_time", c_ulonglong),
+        ("end_time", c_ulonglong),
+        ("response_length", c_int),
+        ("response", c_byte * 4096),
+    ]
+
+
+messenger = CDLL("/project/libtimedmessenger.so")
+messenger.timed_send_and_receive.argtypes = [c_int, c_char_p, c_uint]
+messenger.timed_send_and_receive.restype = TimedResponse
+
+send_and_receive = messenger.timed_send_and_receive
